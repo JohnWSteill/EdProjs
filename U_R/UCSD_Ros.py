ab9198c7abeb95a8e3912bca975fe74441767995
John Steill <John Steill>
2015-12-26 20:24:56 -0600

    Two deBrujin Probs

--- U_R/UCSD_Ros.py
+++ U_R/UCSD_Ros.py
@@ -8,6 +8,14 @@ def writeToOutfile(fun):
                 f.write(a)
     return wrapper
 
+def readInpFileKDna(fun):
+    def wrapper(*args,**kwargs):
+        with open(args[1]) as f:
+            k = int(f.readline())
+            dna = f.readline().strip()
+        ans = fun(*args,dna = dna, k = k)
+    return wrapper
+
 def readInpFileDna(fun):
     def wrapper(*args,**kwargs):
         with open(args[1]) as f:
@@ -15,6 +23,19 @@ def readInpFileDna(fun):
         ans = fun(*args,dna = dna)
     return wrapper
 
+def addKeyAndValTodeBrujin(dB,k,v):
+    if k in dB:
+        dB[k].append(v)
+    else:
+        dB[k] = [v]
+
+def outputDeBrujin(dB):
+    ans = []
+    for k in dB:
+        ans.append(k + ' -> ' + ','.join(dB[k])+'\n')
+    return ans
+    
+    
 # http://docs.sublimetext.info/en/latest/basic_concepts.html
 class UCSD_Ros_Solver(object): 
     """ This class performs Rosalind Project solutions. 
@@ -24,6 +45,25 @@ class UCSD_Ros_Solver(object):
     r.myProb("MyData.txt")
     Assumes that MyData.txt is in working directory or getwd()/Data 
     """
+
+    @readInpFileDna
+    @writeToOutfile
+    def getDeBrujinFromKmers(self,inpFile,dna=None):
+        deBrujin = {}
+        for d in dna:
+            addKeyAndValTodeBrujin(deBrujin, d[:-1],d[1:])
+        return outputDeBrujin(deBrujin)
+
+    @readInpFileKDna
+    @writeToOutfile
+    def getDeBrujinfromText(self,inpFile,k=None,dna=None):
+        deBrujin= {}
+        for i in range(len(dna)-k+1):
+            addKeyAndValTodeBrujin(deBrujin, dna[i:i+k-1],dna[i+1:i+k])
+        return outputDeBrujin(deBrujin)
+        
+        
+            
 
     @readInpFileDna
     @writeToOutfile
--- U_R/runner_script.py
+++ U_R/runner_script.py
@@ -2,5 +2,6 @@ import UCSD_Ros as u
 r = u.UCSD_Ros_Solver()
 #r.StringRecon("Data\dataset_197_3.txt")
 #r.getOverlap('Data/dataset_198_9.txt')
-r.getOverlap(r'Data\dataset_198_9.txt')
+#r.getOverlap(r'Data\dataset_198_9.txt')
 #r.getOverlap('Data/dataset_SampG.txt')
+r.getDeBrujinFromKmers('Data/dataset_200_7.txt')
