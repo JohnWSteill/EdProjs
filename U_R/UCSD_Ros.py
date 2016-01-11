c4ad20542d6e92bd62892afb9bd1e15b5f5c3bc4
John Steill <John Steill>
2016-01-10 21:13:46 -0600

    finished UCSD wk 2

--- U_R/UCSD_Ros.py
+++ U_R/UCSD_Ros.py
@@ -1,3 +1,6 @@
+import pdb
+import random
+
 def writeToOutfile(fun):
     def wrapper(*args,**kwargs):
         ans = fun(*args,**kwargs)
@@ -8,12 +11,47 @@ def writeToOutfile(fun):
                 f.write(a)
     return wrapper
 
+class readInpDecorator():
+    def __init__(self,varList):
+        self.varList = varList
+    def __call__(self, fun):
+        def wrapper(*args, **kwargs):
+            with open(args[1]) as f:
+                inputData = f.readlines()
+            if len(self.varList)==1:
+                kwargs = {self.varList[0]: [el.strip() for el in inputData]}
+            elif len(self.varList[0])==1:
+                kwargs = {self.varList[0]: inputData[0].strip()}
+                kwargs[self.varList[1]] = [el.strip() for el in inputData[1:]]
+            else:
+                kwargs = {el:int(d) for el,d in
+                              zip(self.varList[0],inputData[0].split())}
+                kwargs[self.varList[1]] = [el.strip() for el in inputData[1:]]
+            ans = fun(*args, **kwargs)
+        return wrapper
+                        
+
 def readInpFileKDna(fun):
     def wrapper(*args,**kwargs):
         with open(args[1]) as f:
             k = int(f.readline())
             dna = f.readline().strip()
         ans = fun(*args,dna = dna, k = k)
+    return wrapper
+
+def readInpFileK(fun):
+    def wrapper(*args,**kwargs):
+        with open(args[1]) as f:
+            k = int(f.readline())            
+        ans = fun(*args,k = k)
+    return wrapper
+
+def readInpFileK_Kmers(fun):
+    def wrapper(*args,**kwargs):
+        with open(args[1]) as f:
+            k = int(f.readline())
+            dna = [el.strip() for el in f.readlines()]
+            ans = fun(*args,dna = dna, k = k)
     return wrapper
 
 def readInpFileDirGraph(fun):
@@ -56,18 +94,133 @@ class UCSD_Ros_Solver(object):
     r.myProb("MyData.txt")
     Assumes that MyData.txt is in working directory or getwd()/Data 
     """
-
-    @readInpFileDirGraph
+    @readInpDecorator([['k','d'],'kdmers'])
     @writeToOutfile
-    def getEulerCycle(self, inpFile, g = None):
+    def getOuterStringReconFromPair(self,inpfile,k=None, d = None, kdmers=None):
+        ans = ""
+        while ans == "":
+            ans = self.getStringReconFromPair(k,d,kdmers)
+        return ans
 
-        head = min(g.keys())
-        tail = g[head][0]
-        if len(g[head])>1:
-            g[head] = g[head][1:]
+    def getStringReconFromPair(self,k,d,kdmers):
+        def getHT(kdmerL):
+            for s in kdmerL:
+                head = (s[:k-1],s[k+1:-1])
+                tail = (s[1:k],s[k+2:])
+                yield (head,tail)
+
+        deBrujin = {}
+        for (h,t) in getHT(kdmers):
+            addKeyAndValTodeBrujin(deBrujin,h,t)
+
+            
+        cycle = self.getEulerPath(None,deBrujin)
+        pref = ( cycle[0][0][0] + "".join([el[1][0][-1] for el in cycle]))
+        suff = ( cycle[0][0][1] + "".join([el[1][1][-1] for el in cycle]))
+        #pdb.set_trace()
+        print (len(pref),
+               len([el for el,el1 in zip(pref[k+d:],suff[:-k-d]) if el != el1]))
+        if pref[k+d:] == suff[:-k-d]:
+            return pref + suff[-k-d:]
+            
+        return ""
+
+        
+
+        
+
+    
+    @readInpDecorator([['k','d'],'kdmers'])
+    @writeToOutfile
+    def outerStringSpelledByKdmers(self, inpfile, k=None, d=None, kdmers=None):
+        return self.stringSpelledByKdmers(k,d,kdmers)
+
+    def stringSpelledByKdmers(self,k,d,kdmers):
+        nodeJ = lambda(x):x[0] + "".join(el[-1] for el in x[1:])
+        pref = nodeJ([el[:k] for el in kdmers])
+        suff = nodeJ([el[k+1:] for el in kdmers])
+        pdb.set_trace()
+        if pref[k+d:] == suff[:-k-d]:
+            return pref + suff[-k-d:]
+        return ""      
+            
+
+    @readInpFileK
+    @writeToOutfile
+    def getkCircStr(self,inpfile, k = None):
+        keys = [bin(el)[2:].zfill(k-1) for el in range(2**(k-1))]
+        g = {k:[k[1:]+'0',k[1:]+'1'] for k in keys}
+        cStr = self.getEulerCycle(None, g = g)
+        kmers = cStr.split('->')
+        
+        return kmers[0] + "".join([kmer[-1] for kmer in kmers[1:-k+1]])
+        
+
+    
+    @readInpFileK_Kmers
+    @writeToOutfile
+    def getStringRecon(self, inpfile, k = None, dna = None):
+        deBrujin = {}
+        for d in dna:
+            addKeyAndValTodeBrujin(deBrujin, d[:-1],d[1:])
+        cStr = self.getEulerPath(inpfile, g = deBrujin)
+        kmers = cStr.split('->')
+        return kmers[0] + "".join([kmer[-1] for kmer in kmers[1:]])
+        
+    
+    #@readInpFileDirGraph
+    #@writeToOutfile
+    def getEulerPath(self, inpFile, g = None):
+        edgeImb = {}
+
+        for (h,t) in g.items():
+            if h in edgeImb:
+                edgeImb[h] += len(g[h])
+            else:
+                edgeImb[h] = len(g[h])
+            for t_i in t:
+                if t_i in edgeImb:
+                    edgeImb[t_i] -= 1
+                else:
+                    edgeImb[t_i] = -1
+        head = edgeImb.keys()[edgeImb.values().index(1)]
+        tail = edgeImb.keys()[edgeImb.values().index(-1)]
+        if tail in g:
+            g[tail].append([head])
         else:
+            g[tail] = [head]
+        cycle = self.eulerAlgorithm(g)
+        fakeNodeIndx = cycle.index([tail,head])
+        cycle = cycle[fakeNodeIndx+1:] + cycle[:fakeNodeIndx]
+        return cycle
+        c = [cycle[0][0]]
+        for el in cycle: 
+            c.append(el[1])
+        return '->'.join(c)
+        
+    
+
+    #@readInpFileDirGraph
+    #@writeToOutfile
+    def getEulerCycle(self, inpFile, g = None):
+        cycle = self.eulerAlgorithm(g)
+        c = [cycle[0][0]]
+        for el in cycle:
+            c.append(el[1])
+        return '->'.join(c)
+
+    def getAndRemoveAnyTail(self,g,head):
+        #tail = g[head].pop(random.choice(range(len(g[head]))))
+        tail = g[head].pop()
+        if not g[head]:
             g.pop(head)
-        cycle = [(head,tail)]
+        return tail
+
+    def eulerAlgorithm(self,g):
+        head = min(g.keys())
+        cycle = [(head,self.getAndRemoveAnyTail(g,head))]
+        
+            
         while g != {}:
             tails = [el[1] for el in cycle]
             for k in g.keys():
@@ -77,31 +230,19 @@ class UCSD_Ros_Solver(object):
                 
             indNS = tails.index(newStart)
             cycle = cycle[indNS+1:] + cycle[:indNS+1] 
-            
+
             while True:
                 pT = cycle[-1][1]
                 try:
-                    tail = g[pT][0]
+                    tail = self.getAndRemoveAnyTail(g,pT)
                 except:
                     break
-                if len(g[pT]) > 1:
-                    g[pT] = g[pT][1:]
-                else:
-                    g.pop(pT)
                 cycle.append([pT,tail])
                 
-        
-        c = [cycle[0][0]]
-        for el in cycle:
-            c.append(el[1])
-        return '->'.join(c)
-        
-        
-        
-        
+        return cycle
 
-    @readInpFileKDna
-    @writeToOutfile
+    #@readInpFileKDna
+    #@writeToOutfile
     def getDeBrujinK(self,inpFile,k=None,dna=None):
         kmers = [dna[i:i+k] for i in range(len(dna)-k+1)]
         deBrujin = {}
--- U_R/runner_script.py
+++ U_R/runner_script.py
@@ -1,8 +1,15 @@
 import UCSD_Ros as u
+import pdb
 r = u.UCSD_Ros_Solver()
 #r.StringRecon("Data\dataset_197_3.txt")
 #r.getOverlap('Data/dataset_198_9.txt')
 #r.getOverlap(r'Data\dataset_198_9.txt')
 #r.getDeBrujinFromKmers('Data/dataset_200_7.txt')
-#r.getDeBrujinK('Data/dataset_199_6.txt')
-r.getEulerCycle('Data/dataset_203_2.txt')
+#r.getDeBrujinK('Datra/dataset_199_6.txt')
+#r.getEulerCycle('Data/dataset_203_2.txt')
+#r.getEulerPath(r'Data\dataset_203_5.txt')
+#r.getStringRecon(r'Data\dataset_sampG.txt')
+#r.getStringRecon(r'Data\dataset_203_6.txt')
+#r.getkCircStr(r'Data\dataset_203_10.txt')
+r.getOuterStringReconFromPair(r'Data\dataset_204_14.txt')
+
